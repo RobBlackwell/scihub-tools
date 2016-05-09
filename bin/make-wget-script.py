438eef292d2b4bf77e074ab107c057d906bc7d84
Rob Blackwell <rob.blackwell@robblackwell.org.uk>
2016-05-09 22:12:55 +0100

    Beatrice example

--- bin/make-wget-script.py
+++ bin/make-wget-script.py
@@ -4,27 +4,26 @@ import sys
 from optparse import OptionParser
 
 # You might consider putting your scihub credentials in ~/.netrc
-    
+
 def wget_command (username, password, uuid, filename):
     user_option = ''
     password_option = ''
     filename_option = ''
-    
+
     if filename is not None:
         filename_option = ' -O {}.zip'.format(filename)
-        
+
     if username is not None:
         user_option = ' -user={}'.format(username)
 
     if password is not None:
         password_option = ' --passwrod={}'.format(password)
-        
+
     s = 'wget --continue{}{} "https://scihub.copernicus.eu/apihub/odata/v1/Products(\'{}\')/\$value"{}'.format(user_option, password_option, uuid, filename_option)
 
-    print s
+    print (s)
 
 
-            
 def toploop(username, password):
 
     filename = None
@@ -37,7 +36,7 @@ def toploop(username, password):
         if len(x) > 1:
             filename = x[1]
         wget_command (username, password, uuid, filename)
-    
+
 
 if __name__ == "__main__":
 
@@ -50,7 +49,7 @@ if __name__ == "__main__":
                       help='UUID of a scihub product.')
     parser.add_option('-O', '--output', dest='output',
                       help='Output filename.')
-      
+
     (options, args) = parser.parse_args()
 
     username = options.user
---
+++ examples/beatrice.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+../bin/scihub-search.py -q "productType:GRD AND footprint:\"Intersects(58.0924, -3.1533)\" AND beginposition:[2016-01-01T00:00:00.000Z TO 2016-01-08T00:00:00.000Z]" 
+
+
+
