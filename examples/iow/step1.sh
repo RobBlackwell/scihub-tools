e91b8a380844fa2b2e9ac8d677ee6b44ddf5ee5a
Rob Blackwell <rob.blackwell@robblackwell.org.uk>
2016-05-20 21:33:36 +0100

    curl

--- examples/iow/step1.sh
+++ examples/iow/step1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-../../bin/scihub-search.py -q "productType:GRD AND footprint:\"Intersects(POLYGON((-1.66 50.56, -1.04 50.56, -1.04 50.77, -1.66 50.77, -1.66 50.56)))\"&rows=1" | ../../bin/make-wget-script.py > step2.sh
+../../bin/scihub-search.py -q "productType:GRD AND footprint:\"Intersects(POLYGON((-1.66 50.56, -1.04 50.56, -1.04 50.77, -1.66 50.77, -1.66 50.56)))\"&rows=1" | ../../bin/make-curl-script.py > step2.sh
 
 
